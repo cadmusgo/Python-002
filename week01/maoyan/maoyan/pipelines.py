ec07a0a358811036d8d3485e374af4e125c843c9
Cadmus <cadmus.go@gmail.com>
2020-07-30 21:46:55 +0800

    [fix] item type name error

--- week01/maoyan/maoyan/pipelines.py
+++ week01/maoyan/maoyan/pipelines.py
@@ -9,7 +9,7 @@
 class MaoyanPipeline:
     def process_item(self, item, spider):
         with open('./movie-spider.csv', 'a+', encoding='utf-8') as f:
-            print(f'{item["title"]},{item["movie_type"]},{item["movie_time"]}')
-            f.write(f'{item["title"]},{item["movie_type"]},{item["movie_time"]}\n')
+            print(f'{item["title"]},{item["type"]},{item["time"]}')
+            f.write(f'{item["title"]},{item["type"]},{item["time"]}\n')
 
         return item
