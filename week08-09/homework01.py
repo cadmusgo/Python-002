cedacbcb3b18223be1586411f2004e9548f42f1f
Cadmus <cadmus.go@gmail.com>
2020-09-16 16:54:57 +0800

    ok

--- week08-09/homework01.py
+++ week08-09/homework01.py
@@ -1,19 +1,7 @@
-"""
-    哪些是谝平序列?
-"""
-str
+# 谝平序列 : str
 
-"""
-    哪些是可變序列? MutableSequence
-"""
-list
-dict
-collections.deque
+# 容器序列: list,tuple,dict,collections.deque
 
-# list
+# 可变序列：list,dict,collections.deque
 
-"""
-    哪些是不可變序列? sequence
-"""
-tuple
-str
+# 不可变序列：str,tuple
